23b531991f973d030dc6cf9d6333a86426cc7d9d
Virtual Jasper <jasper2virtual@hotmail.com>
2025-01-07 23:32:02 +0800

    refactor: clean up code and improve mobile menu functionality

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <header class="flex fixed top-0 left-0">
+  <header class="flex items-center fixed top-0 left-0 w-full p-4">
     <div>
       <img src="/src/assets/images/logo.svg" alt="logo" />
     </div>
     <div class="hidden app-desktop:block ml-auto">
       <desktop-menu :menu-data="menuData" />
     </div>
-    <div class="app-desktop:hidden ml-auto">
+    <div class="app-desktop:hidden ml-auto h-14 flex items-center">
       <mobile-menu :menu-data="menuData" />
     </div>
   </header>
-  <main></main>
+  <main class="p-4 mt-[5.5rem]">
+  </main>
 </template>
 <script lang="ts" setup>
 import { ref } from 'vue'
 import DesktopMenu from './components/desktop-menu.vue'
 import MobileMenu from './components/mobile-menu.vue'
 const menuData = ref([
-  { text: "Home", path: "/home" },
-  { text: "New", path: "/new" },
-  { text: "Popular", path: "/popular" },
-  { text: "Trending", path: "/trending" },
-  { text: "Categories", path: "/categories" }
+  { text: "Home", path: "./home" },
+  { text: "New", path: "./new" },
+  { text: "Popular", path: "./popular" },
+  { text: "Trending", path: "./trending" },
+  { text: "Categories", path: "./categories" }
 ])
 </script>
--- src/components/mobile-menu.vue
+++ src/components/mobile-menu.vue
@@ -2,14 +2,14 @@
     <button v-if="!isOpen" class="hambuger" @click="openMenu">
         <img src="/src/assets/images/icon-menu.svg" alt="menu" />
     </button>
-    <button v-if="isOpen" class="hambuger self-end" @click="closeMenu">
+    <button v-else class="hambuger" @click="closeMenu">
         <img src="/src/assets/images/icon-menu-close.svg" alt="close" />
     </button>
 
     <transition>
-        <div v-if="isOpen" class="bg-black/50 fixed h-screen w-screen top-0 left-0">
+        <div v-if="isOpen" class="bg-black/50 fixed h-full w-full top-0 left-0 -z-10">
             <div class="flex flex-col p-4 w-8/12 ml-auto bg-app-almost-white h-full">
-                <div class="h-full max-h-16"></div>
+                <div class="h-full max-h-32"></div>
                 <nav class="app-text-heading-xs text-app-dark-space-blue font-normal flex flex-col gap-6">
                     <li v-for="menuItem in menuData" class="list-none">
                         <a :href="menuItem.path">{{ menuItem.text }}</a>
